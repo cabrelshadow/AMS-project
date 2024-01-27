9b7f025fac2fc1c5e96c28b4f4f73ef01f4ed766
francis-tg <franc9885@gmail.com>
2024-01-27 20:27:20 +0000

    start add relationship

--- models/bon_livraison.js
+++ models/bon_livraison.js
@@ -1,27 +1,32 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Bon_livraison extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Bon_livraison.init({
-    code: DataTypes.STRING,
-    qty: DataTypes.INTEGER,
-    truck_number: DataTypes.STRING,
-    destination: DataTypes.STRING,
-    client_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Bon_livraison',
-  });
-  return Bon_livraison;
+	class Bon_livraison extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.belongsTo(models.Client, {
+				foreign_key: "client_id",
+				onDelete: "CASCADE",
+			});
+		}
+	}
+	Bon_livraison.init(
+		{
+			code: DataTypes.STRING,
+			qty: DataTypes.INTEGER,
+			truck_number: DataTypes.STRING,
+			destination: DataTypes.STRING,
+			client_id: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: "Bon_livraison",
+		},
+	);
+	return Bon_livraison;
 };
--- models/clients.js
+++ models/clients.js
@@ -1,26 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Clients extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Clients.init({
-    name: DataTypes.STRING,
-    contact: DataTypes.STRING,
-    email: DataTypes.STRING,
-    location: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Clients',
-  });
-  return Clients;
+	class Clients extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.hasMany(models.Bon_livraison, {
+				foreign_key: "client_id",
+				onDelete: "CASCADE",
+			});
+		}
+	}
+	Clients.init(
+		{
+			name: DataTypes.STRING,
+			contact: DataTypes.STRING,
+			email: DataTypes.STRING,
+			location: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: "Clients",
+		},
+	);
+	return Clients;
 };
