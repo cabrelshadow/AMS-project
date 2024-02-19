d90cfe8e05f635d131a0a48ed27206c691bc2c2b
mr shadow <sianou93@gmail.com>
2024-02-19 17:58:08 +0100

    add view colie(reconditioner

--- .history/server_20240217080350.js
+++ .history/server_20240217080350.js
@@ -104,7 +104,6 @@ app.use("/colie-type", require("./routes/colie_type"));
 
 app.use("/auth", require("./routes/auth"));
 app.use("/categorie", require("./routes/categorie"));
-app.use("/recondition", require("./routes/Colie_reconditionner"));
 
 http.createServer(app).listen(4500, () => {
 	console.log(`server run on port 4500`);
